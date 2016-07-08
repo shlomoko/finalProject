c1061639c554564582a024b4f29852f43b4e16cf
rotemge <rotemgera@gmail.com>
2016-07-08 15:16:15 +0300

    In "block an app" activity - radio buttons dis/enables controls

--- app/src/main/java/il/ac/huji/phonetime/BlockAnAppActivity.java
+++ app/src/main/java/il/ac/huji/phonetime/BlockAnAppActivity.java
@@ -5,9 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class BlockAnAppActivity extends AppCompatActivity {
+
+    private EditText timeAmount;
+    private Spinner timeFrameSpinner;
+    private Spinner timeUnitsSpinner;
+    private EditText startTime;
+    private EditText endTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +24,12 @@ public class BlockAnAppActivity extends AppCompatActivity {
         setContentView(R.layout.activity_block_an_app);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setComponents();
+        setConfirm();
+        setRadio();
+    }
 
+    private void setConfirm(){
         FloatingActionButton okButton = (FloatingActionButton) findViewById(R.id.confirm);
         if (okButton != null) {
             okButton.setOnClickListener(new View.OnClickListener() {
@@ -29,4 +43,30 @@ public class BlockAnAppActivity extends AppCompatActivity {
         }
     }
 
+    private void setComponents(){
+        timeAmount = (EditText)findViewById(R.id.time_amount);
+        timeFrameSpinner = (Spinner)findViewById(R.id.timeFrameSpinner);
+        timeUnitsSpinner = (Spinner)findViewById(R.id.timeUnitsSpinner);
+        startTime = (EditText)findViewById(R.id.start_time);
+        endTime = (EditText)findViewById(R.id.end_time);
+    }
+
+
+    private void setRadio(){
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.rb_group);
+        if (radioGroup != null) {
+            radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+                    boolean cond = (checkedId == R.id.rb_after);
+                    timeAmount.setEnabled(cond);
+                    timeFrameSpinner.setEnabled(cond);
+                    timeUnitsSpinner.setEnabled(cond);
+                    startTime.setEnabled(!cond);
+                    endTime.setEnabled(!cond);
+                }
+            });
+            radioGroup.check(R.id.rb_after);
+        }
+    }
 }
