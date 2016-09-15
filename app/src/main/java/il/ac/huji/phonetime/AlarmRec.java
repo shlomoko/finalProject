465145cce306e8521dcbfe91b4782e31ad27e0b1
shlomoko <shlomokoppel2@gmail.com>
2016-09-16 01:02:47 +0300

    broken

--- app/src/main/java/il/ac/huji/phonetime/AlarmRec.java
+++ app/src/main/java/il/ac/huji/phonetime/AlarmRec.java
@@ -3,21 +3,31 @@ package il.ac.huji.phonetime;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.display.DisplayManager;
+import android.view.Display;
 
 /**
  * Created by Shlomo on 28/07/2016.
  */
 public class AlarmRec extends BroadcastReceiver {
     public static final int REQUEST_CODE = 12345;
-//    public static final String ACTION = "com.codepath.example.servicesdemo.alarm";
+    public static final String ACTION = "com.codepath.example.servicesdemo.alarm";
 
     // Triggered by the Alarm periodically (starts the service to run task)
     @Override
     public void onReceive(Context context, Intent intent) {
         Intent i = new Intent(context, CheckRunningApp.class);
         i.putExtra("foo", "bar");
-        if (intent.getAction().equals(intent.ACTION_SCREEN_ON)) {
-            context.startService(i);
-        }
+        context.startService(i);
     }
+
+//    private boolean checkScreenOn(Context context){
+//        DisplayManager dm = (DisplayManager) context.getSystemService(Context.DISPLAY_SERVICE);
+//        for (Display display : dm.getDisplays()) {
+//            if (display.getState() != Display.STATE_OFF) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 }
